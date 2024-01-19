323ffc3f2d58921a9d166823487796f7a6f3c5dc
totigm <totigm@gmail.com>
2024-01-19 02:22:19 -0300

    🚧 Add dot notation
    JS files are working correctly with dot notation, but this has broken TS files

--- src/styled.js
+++ src/styled.js
@@ -1,25 +1,48 @@
 import React from 'react';
-
 import { cache } from './components/StyleRegistry';
 
-// TODO: Ideally, this API would use dot notation (styled.div) in
-// addition to function calls (styled('div')). We should be able to
-// use Proxies for this, like Framer Motion does.
-export default function styled(Tag) {
-  return (css) => {
-    return function StyledComponent(props) {
-      let collectedStyles = cache();
+/**
+ * @typedef {(css: string) => React.FunctionComponent<React.HTMLProps<HTMLElement>>} StyledFunction
+ * @typedef {Record<keyof JSX.IntrinsicElements, StyledFunction>} StyledInterface
+ */
 
-      // Instead of using the filename, I'm using the `useId` hook to
-      // generate a unique ID for each styled-component.
-      const id = React.useId().replace(/:/g, '');
-      const generatedClassName = `styled-${id}`;
+/**
+ * Creating a Proxy around the `styled` function.
+ * This allows us to intercept property accesses (like styled.div)
+ * and treat them as function calls (like styled('div')).
+ *
+ * @type {StyledInterface}
+ */
+const styled = new Proxy(
+  function (Tag) {
+    // The original styled function that creates a styled component
+    return (css) => {
+      return function StyledComponent(props) {
+        let collectedStyles = cache();
 
-      const styleContent = `.${generatedClassName} { ${css} }`;
+        // Using the `useId` hook to generate a unique ID for each styled-component.
+        const id = React.useId().replace(/:/g, "");
+        const generatedClassName = `styled-${id}`;
 
-      collectedStyles.push(styleContent);
+        const styleContent = `.${generatedClassName} { ${css} }`;
 
-      return <Tag className={generatedClassName} {...props} />;
+        collectedStyles.push(styleContent);
+
+        return <Tag className={generatedClassName} {...props} />;
+      };
     };
-  };
-}
+  },
+  {
+    get: function (target, prop) {
+      // Intercepting property access on the `styled` function.
+      if (typeof prop === 'string') {
+        // If the property is a string, call the original `styled` function with the property name as the tag.
+        return target(prop);
+      }
+      // Default behavior for other properties
+      return Reflect.get(target, prop);
+    },
+  }
+);
+
+export default styled;
