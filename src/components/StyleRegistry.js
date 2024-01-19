5d47db247d0e032289dff2c8da2771a835ed63f5
totigm <totigm@gmail.com>
2024-01-19 11:11:51 -0300

    🐛 Sort classes

--- src/components/StyleRegistry.js
+++ src/components/StyleRegistry.js
@@ -6,11 +6,24 @@ export const cache = React.cache(() => {
   return [];
 });
 
+const sortFn = (a, b) => {
+  const aClassCount = a.fullClassName.split(' ').length;
+  const bClassCount = b.fullClassName.split(' ').length;
+
+  if (aClassCount === bClassCount) {
+    // If they have the same class count, maintain the original order.
+    return 0;
+  } else {
+    // Sort by the number of classes in ascending order.
+    return aClassCount - bClassCount;
+  }
+}
+
 function StyleRegistry({ children }) {
   const collectedStyles = cache();
   const stylesMap = React.useMemo(
     () =>
-      collectedStyles.map(({ className, css }) => `.${className} { ${css} }`),
+      collectedStyles.sort(sortFn).map(({ className, css }) => `.${className} { ${css} }`),
     [collectedStyles]
   );
 
