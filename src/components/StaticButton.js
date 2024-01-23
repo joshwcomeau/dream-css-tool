ddcfc9dde2d628bd95f62a2326b213b6e7881d33
Toti Muñoz <64804554+totigm@users.noreply.github.com>
2024-01-23 06:53:47 -0300

    ✨ Add props interpolation (#1)

--- src/components/StaticButton.js
+++ src/components/StaticButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from '../styled.js';
 
 export default function StaticButton() {
-  return <Button>Static Button</Button>;
+  return <Button primary>Static Button</Button>;
 }
 
 const Button = styled.button`
@@ -10,7 +10,8 @@ const Button = styled.button`
   padding: 1rem 2rem;
   border: none;
   border-radius: 4px;
-  background: hsl(270deg 100% 30%);
+  background: ${(props) =>
+    props.primary ? 'hsl(270deg 100% 30%)' : 'hsl(180deg 100% 30%)'};
   color: white;
   font-size: 1rem;
   cursor: pointer;
